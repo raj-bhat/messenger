32f148ac3ede6ff8e0d179af3cc5d613afbf2700
rbhat <bhatrajshekhar@gmail.com>
2018-10-08 20:49:02 +0530

    1st push

---
+++ src/main/java/org/raj/test/messenger/resources/MessageResource.java
@@ -0,0 +1,20 @@
+package org.raj.test.messenger.resources;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("/messages")
+public class MessageResource {
+	
+	
+	@GET
+	@Produces(MediaType.TEXT_PLAIN)
+	public String getMessages()
+	{
+		return "hello World";
+	}
+
+}
---
+++ src/main/java/org/raj/test/messenger/service/MessageService.java
@@ -0,0 +1,6 @@
+package org.raj.test.messenger.service;
+
+public class MessageService {
+
+	
+}
